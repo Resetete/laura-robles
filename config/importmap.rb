28d7ea5ae3589a547b58ecc6229a499a1a138ecc
Theresa <tmannschatz@gmail.com>
2022-07-06 15:36:32 +0200

    Install jquery

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 # Bootstrap and dependencies
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.2/dist/esm/index.js" # use unpkg.com as ga.jspm.io contains a broken popper package
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js"
