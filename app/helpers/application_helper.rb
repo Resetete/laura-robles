7ad76d997ab1f5dd73f0f1e01908ec5700ad0f9f
Theresa <tmannschatz@gmail.com>
2022-07-27 23:30:57 +0200

    Style footer and add contact link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def contact_helper
+    link_to('contact', 'mailto:lauraroblesinfo@gmail.com', class: 'contact-link')
+  end
+
+  def social_media_helper
+    tag.p('social media')
+  #   tag.div(class: 'social-media fa-2x fa-fw')
+  #     = link_to '#' do
+  #       %i.fa-brands.fa-facebook-square.fa-flip
+  #     = link_to '#' do
+  #       %i.fa-brands.fa-instagram-square.fa-flip
+  #     = link_to '#' do
+  #       %i.fa-brands.fa-solid.fa-square-envelope.fa-flip
+  end
 end
