d40e991f043746e514c327d83f2939ce8b8579b2
Theresa <tmannschatz@gmail.com>
2022-07-30 21:48:40 +0200

    Move language selection to view helper; create social media helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,14 +3,31 @@ module ApplicationHelper
     link_to('contact', 'mailto:lauraroblesinfo@gmail.com', class: 'contact-link')
   end
 
-  def social_media_helper
-    tag.p('social media')
-  #   tag.div(class: 'social-media fa-2x fa-fw')
-  #     = link_to '#' do
-  #       %i.fa-brands.fa-facebook-square.fa-flip
-  #     = link_to '#' do
-  #       %i.fa-brands.fa-instagram-square.fa-flip
-  #     = link_to '#' do
-  #       %i.fa-brands.fa-solid.fa-square-envelope.fa-flip
+  def social_media_helper(animation:, direction:)
+    tag.div(class: "social-media-#{direction} fa-2x #{'fa-fw' if direction == 'vertical'}") do
+      [
+        (link_to '#' do
+          tag.i(class: "fa-brands fa-facebook-square #{animation}")
+        end),
+        (link_to '#' do
+          tag.i(class: "fa-brands fa-instagram-square #{animation}")
+        end),
+        (link_to '#' do
+          tag.i(class: "fa-brands fa-solid fa-square-envelope #{animation}")
+        end)
+      ].join.html_safe
+    end
+  end
+
+  def language_selection
+    tag.div(class: 'language-selection') do
+      [
+        link_to('DE', '#'),
+        '/',
+        link_to('EN', '#'),
+        '/',
+        link_to('ES', '#')
+      ].join(' ').html_safe
+    end
   end
 end
