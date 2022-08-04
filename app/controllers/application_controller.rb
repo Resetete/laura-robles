4821b0d77b9e0b396af25a0096c4efe881d825ea
Theresa <tmannschatz@gmail.com>
2022-08-04 23:34:38 +0200

    Add locale params to url in routes and add before_action which will set the locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = extract_locale || I18n.default_locale
+  end
+
+  def extract_locale
+    parsed_locale = params[:locale]
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  root 'pages#home'
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    root 'pages#home'
+  end
 end
