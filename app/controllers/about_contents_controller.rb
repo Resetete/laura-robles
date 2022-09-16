04f92c5873786bbf5adfbe98e79b1fa2d43baf3b
Theresa <tmannschatz@gmail.com>
2022-09-16 14:55:22 +0200

    Add About Content Index page and create new form

--- app/controllers/about_contents_controller.rb
+++ app/controllers/about_contents_controller.rb
@@ -2,4 +2,26 @@ class AboutContentsController < ApplicationController
   def index
     @contents = AboutContent.all
   end
+
+  def new
+    @content = AboutContent.new
+  end
+
+  def create
+    @content = AboutContent.new(about_content_params)
+    if @content.save
+      redirect_to about_contents_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def about_content_params
+    params.require(:about_content).permit(
+      part_1: [:en, :es, :de],
+      part_2: [:en, :es, :de],
+    )
+  end
 end
--- app/models/about_content.rb
+++ app/models/about_content.rb
@@ -2,7 +2,7 @@ class AboutContent < ApplicationRecord
   # Format of the part_1 and part_2 entries
   # { en: 'english about text', de: 'deutscher about text', es: 'texto em espanhol' }
 
-  def version_for(part: 'part_1', locale:)
-    public_send(part)&.fetch(locale)
+  def version_for(part:, locale:)
+    public_send(part).to_h&.fetch(locale.to_s)
   end
 end
