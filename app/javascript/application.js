5dfc50d3e1cb514e818639ca73ff5400577c1356
Theresa <tmannschatz@gmail.com>
2022-10-07 14:34:01 +0200

    Add cookie banner with translations

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -213,4 +213,15 @@ animate();
 };
 
 
+$(document).ready(function(){
+// cookie banner
+  if(localStorage.getItem('cookieSeen') != 'shown'){
+    $(".cookie-banner").delay(2000).fadeIn();
+    localStorage.setItem('cookieSeen','shown')
+  }
+
+  $('.close').click(function(e) {
+    $('.cookie-banner').fadeOut();
+  });
+});
 
