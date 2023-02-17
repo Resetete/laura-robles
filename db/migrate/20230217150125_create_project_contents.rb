dcc508140d45d3331a9d4b129903140f0a65aadb
Theresa <tmannschatz@gmail.com>
2023-02-17 22:54:32 +0100

    Update project table migration

--- db/migrate/20230217150125_create_project_contents.rb
+++ db/migrate/20230217150125_create_project_contents.rb
@@ -1,14 +1,14 @@
 class CreateProjectContents < ActiveRecord::Migration[7.0]
   def change
     create_table :project_contents do |t|
-      t.string :title
+      t.string :title, null: false
 
-      t.text :description_en
-      t.text :description_es
-      t.text :description_de
+      t.text :description_en, null: false
+      t.text :description_es, null: false
+      t.text :description_de, null: false
 
-      t.string :music_url
-      t.string :image_url
+      t.string :music_url, null: true
+      t.string :image_url, null: true
 
       t.timestamps
     end
