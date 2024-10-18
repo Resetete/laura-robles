3a490a5d842c8cb2d6cae6ac96b7ec694e993d05
Theresa <tmannschatz@gmail.com>
2024-10-18 17:38:28 +0200

    Comment the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,30 +7,30 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 # Initialize contents, version from 18.10.2024
-AboutContent.create(
-  part_1: {
-    en: 'english about text',
-    de: 'deutscher about text',
-    es: 'texto em espanhol',
-  },
-  part_2: {
-    en: 'english about text',
-    de: 'deutscher about text',
-    es: 'texto em espanhol'
-  },
-  active: true,
-)
+# AboutContent.create(
+#   part_1: {
+#     en: 'english about text',
+#     de: 'deutscher about text',
+#     es: 'texto em espanhol',
+#   },
+#   part_2: {
+#     en: 'english about text',
+#     de: 'deutscher about text',
+#     es: 'texto em espanhol'
+#   },
+#   active: true,
+# )
 
-ImprintContent.create(
-  copyright_en: 'copyright en',
-  copyright_de: 'copyright de',
-  copyright_es: 'copyright es',
-)
+# ImprintContent.create(
+#   copyright_en: 'copyright en',
+#   copyright_de: 'copyright de',
+#   copyright_es: 'copyright es',
+# )
 
-ProjectContent.create(
-  title: 'test project',
-  description_en: 'description_en',
-  description_de: 'description_de',
-  description_es: 'description_es',
-  musicians: 'A number of known musicians',
-)
+# ProjectContent.create(
+#   title: 'test project',
+#   description_en: 'description_en',
+#   description_de: 'description_de',
+#   description_es: 'description_es',
+#   musicians: 'A number of known musicians',
+# )
